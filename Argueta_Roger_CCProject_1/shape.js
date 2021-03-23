1172887d63dca673c617f948251ad0071fe3d20c
RogA569 <raa569@nyu.edu>
2021-03-22 23:27:45 -0400

    Integrate cannon into Shape class file

--- Argueta_Roger_CCProject_1/shape.js
+++ Argueta_Roger_CCProject_1/shape.js
@@ -1,27 +1,63 @@
 class Shape {
 
-	// BRING IN THE CANNONS!!!
-
 	constructor(tc) {
 		//if (tc) { // circle
 			//
 		//}
 		if (tc != true) { // triangle
-			this.triangle_a_x = (width/2) - 50;
-			this.triangle_a_y = (height/2) + 50;
-			this.triangle_b_x = (width/2) + 50;
-			this.triangle_b_y = (height/2) + 50;
-			this.triangle_c_x = (width/2);
-			this.triangle_c_y = (height/2) - 25;
+			this.triangle_a_x = -50;
+			this.triangle_a_y =  50;
+			this.triangle_b_x = 50;
+			this.triangle_b_y = 50;
+			this.triangle_c_x = 0;
+			this.triangle_c_y = -25;
+
+			this.cannon_move = 0;
 		}
 	}
 
-	display() {
-		beginShape();
-			fill(127);
-			vertex(this.triangle_a_x, this.triangle_a_y);
-			vertex(this.triangle_b_x, this.triangle_b_y);
-			vertex(this.triangle_c_x, this.triangle_c_y);
-		endShape(CLOSE);
+	display() { //after cannons (and their methods) are set, put them before shape is created
+		push();
+			translate(width/2, height/2);
+			beginShape();
+				fill(127);	
+				vertex(this.triangle_a_x, this.triangle_a_y);
+				vertex(this.triangle_b_x, this.triangle_b_y);
+				vertex(this.triangle_c_x, this.triangle_c_y);
+			endShape(CLOSE);
+		pop();
+
+		//top cannon
+		fill(0);
+		push();
+			translate(width/2 + this.triangle_c_x, height/2 + this.triangle_c_y);
+			ellipse(0, (15 - this.cannon_move), 10, 10);
+			ellipse(0, (9 - this.cannon_move), 9, 3);
+		pop();
+
+		// left cannon
+		fill(0);
+		push();
+			translate(width/2 + this.triangle_a_x, height/2 + this.triangle_a_y);
+			rotate(radians(-120));
+			ellipse(0, (15 - this.cannon_move), 10, 10);
+			ellipse(0, (9 - this.cannon_move), 9, 3);
+		pop();
+
+		//right cannon
+		fill(0);
+		push();
+			translate(width/2 + this.triangle_b_x, height/2 + this.triangle_b_y);
+			rotate(radians(120));
+			ellipse(0, (15 - this.cannon_move), 10, 10);
+			ellipse(0, (9 - this.cannon_move), 9, 3);
+		pop();
+	}
+
+	cannon_motion() {
+		// method which makes cannons protrude from vertices
+		while (this.cannon_move <= 15) {
+			this.cannon_move += 0.0000001;
+		}
 	}
 }
--- Argueta_Roger_CCProject_1/sketch.js
+++ Argueta_Roger_CCProject_1/sketch.js
@@ -1,35 +1,16 @@
 let triangle_or_circle;
 let protagonist_shape;
-let top_cannon;
 
 function setup() {
 	createCanvas(900, 900);
 	triangle_or_circle = false;
 	protagonist_shape = new Shape(triangle_or_circle);
-	top_cannon = new Cannon(width/2, height/2 - 25);
 }
 
 function draw() {
 	background(255);
-	top_cannon.display();
 	protagonist_shape.display();
-	top_cannon.display();
+	//find a way to slow down this motion...
+	protagonist_shape.cannon_motion();
 }
-
-
-class Cannon {
-	// what if the cannons are on the side vertices?
-	constructor(v_x, v_y) {
-		
-		this.pos_x = v_x;
-		this.pos_y = v_y + 15;
-	}
-
-	display() {
-		fill(0);
-		ellipse(this.pos_x, this.pos_y, 10, 10);
-		ellipse(this.pos_x, this.pos_y-6, 9, 3);
-	}
-}
-
 
